1c03a946b7e8143f9cc9fdb604cae73a191cd613
Aziz Khoury <bentael@gmail.com>
2013-09-20 01:19:13 -0400

    adding beer, about, terms pages files

--- src/server/server.js
+++ src/server/server.js
@@ -5,10 +5,19 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
     res.render('index/index', {title: 'Home'});
+});
+app.get('/beer', function(req, res){
+    res.render('index/beer', {title: 'Beer'});
+});
+app.get('/terms', function(req, res){
+    res.render('index/terms', {title: 'Terms'});
+});
+app.get('/about', function(req, res){
+    res.render('index/about', {title: 'About'});
 });
 
 port = port || 3000;
-console.log("server running on port: " + port);
+console.log('server running on port: ' + port);
 app.listen(port);
