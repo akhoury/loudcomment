67e4d574dad263e833888eaac54cf76a921ca9a9
Aziz Khoury <bentael@gmail.com>
2013-09-20 01:21:20 -0400

    lower casing some stuff

--- src/server/server.js
+++ src/server/server.js
@@ -6,16 +6,16 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.render('index/index', {title: 'Home'});
+    res.render('index/index', {title: 'home'});
 });
 app.get('/beer', function(req, res){
-    res.render('index/beer', {title: 'Beer'});
+    res.render('index/beer', {title: 'beer'});
 });
 app.get('/terms', function(req, res){
-    res.render('index/terms', {title: 'Terms'});
+    res.render('index/terms', {title: 'terms'});
 });
 app.get('/about', function(req, res){
-    res.render('index/about', {title: 'About'});
+    res.render('index/about', {title: 'about'});
 });
 
 port = port || 3000;
